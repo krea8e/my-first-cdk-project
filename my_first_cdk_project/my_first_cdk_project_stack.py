187846c2d59d05c2ff10a0a18249a252087f660b
Souvik Dutta <souvik.coder@gmail.com>
2021-08-19 08:49:06 -0700

    Created an S3 Bucket and Export the bucket name

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -18,5 +18,24 @@ class MyFirstCdkProjectStack(cdk.Stack):
         # The code that defines your stack goes here
         _s3.Bucket(
             self, 
-            "myBucketId"
+            "myBucketId",
+            bucket_name="myfirstcdkproject20210819",
+            versioned=False,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
+        )
+
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketId1"
+        )
+
+#Create an output and export so that other stacks can use them
+
+        output_1 = cdk.CfnOutput(
+            self,
+            "myBucketOutput1",                  #Output Key
+            value=mybucket.bucket_name,         #Output Value
+            description=f"My first CDK Bucket",
+            export_name="myBucketOutput1"       #Export name that is available in Export tab in CloudFormation
         )
